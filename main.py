4bfd2cdc95cbd3a8ae07b9180da3db7d2cd10a05
Kraego <tkraxner@gmx.at>
2020-10-29 15:15:23 +0100

    further progress, something wrong in extracting eigenval

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 import json
 from plotter import Plotter
-from movement_calculator import calculate_positions, calculate_error_ellipse
+from movement_calculator import Movement, POS_Y, POS_X, POS_MOVED, POS_ALPHA
 
 CONFIG_FILE = './movingConfig.json'
+
+
+def __init_plot_area():
+    plot_area = data['plot_area']
+    Plotter.init(plot_area['xmin'], plot_area['xmax'],
+                 plot_area['ymin'], plot_area['ymax'])
+
+
+def __plot_add_start_point(x, y):
+    Plotter.add_position(x, y)
+
+
+def __plot_add_positions_and_failure_ellipses(start_pos, steps, uncertainty_factors, wheel_distance):
+    positions = Movement.calculate_positions(start_pos, steps)
+
+    for pos in positions:
+        Plotter.add_position(pos[POS_X], pos[POS_Y])
+        error_ellipse = Movement.calculate_error_ellipse(uncertainty_factors,
+                                                         wheel_distance, pos[POS_MOVED], pos[POS_ALPHA])
+
+        Plotter.add_ellipse(pos[POS_X], pos[POS_Y], error_ellipse['width'], error_ellipse['height'],
+                            error_ellipse['angle'])
 
 
 if __name__ == '__main__':
@@ -11,17 +33,7 @@ if __name__ == '__main__':
     with open(CONFIG_FILE) as f:
         data = json.load(f)
 
-    plot_area = data['plot_area']
-    Plotter.init(plot_area['xmin'], plot_area['xmax'],
-                 plot_area['ymin'], plot_area['ymax'])
-
-    Plotter.add_position(data['start_position']['x'], data['start_position']['y'])
-    positions = calculate_positions(data['start_position'], data['steps'])
-
-    for pos in positions:
-        Plotter.add_position(pos['x'], pos['y'])
-        error_ellipse = calculate_error_ellipse(data['drive_uncertainty_factors'],
-                                                data['wheel_distance'], pos['alpha'])
-        Plotter.add_ellipse(pos['x'], pos['y'], error_ellipse['width'], error_ellipse['height'],
-                            error_ellipse['angle'])
+    __init_plot_area()
+    __plot_add_start_point(data['start_position']['x'], data['start_position']['y'])
+    __plot_add_positions_and_failure_ellipses(data['start_position'], data['steps'], data['drive_uncertainty_factors'], data['wheel_distance'])
     Plotter.plot()
--- movement_calculator.py
+++ movement_calculator.py
@@ -1,62 +1,59 @@
 import math
+from failure_model import calculate_sigma_p_n_1, extract_ellipse
 
-import numpy as np
-
-global last_covariance_matrix
-
-
-def calculate_positions(start_position, steps):
-    """
-    Calculate position regarding to taken steps/rotations
-
-    :param start_position: start position (dict with 'x' & 'y')
-    :param steps: movement, a dict with
-                    ('move' ... movement in x direction of moving robot
-                     'rotation_degrees' ... rotation in degrees)
-    :return: a list of positions: position is dict with 'x', 'y' and 'alpha'
-    """
-    alpha_rads = 0
-    previous_x = start_position['x']
-    previous_y = start_position['y']
-    positions = []
-
-    for step in steps:
-        alpha_rads = alpha_rads + math.radians(step['rotation_degrees'])
-        position = dict()
-        position['x'] = previous_x + step['move'] * math.cos(alpha_rads)
-        position['y'] = previous_y + step['move'] * math.sin(alpha_rads)
-        position['alpha'] = alpha_rads
-        positions.append(position)
-        previous_x = position['x']
-        previous_y = position['y']
-
-    return positions
+# moved distance to get to actual pos
+POS_MOVED = 'movement'
+# intended Rotation
+POS_ALPHA = 'alpha'
+POS_Y = 'y'
+POS_X = 'x'
+# input data
+STEP_MOVE = 'move'
+STEP_ROT_DEGREES = 'rotation_degrees'
 
 
-def calculate_error_ellipse(drive_uncertainty_factors, wheel_distance, alpha):
-    """
-    Calculate error ellipse
+class Movement:
+    sigma_p_n = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-    :param drive_uncertainty_factors: the factors
-    :param wheel_distance: wheel distance
-    :param alpha: rotation
-    :return: a dict containing 'width', 'height' and 'angle'
-    """
-    sigma_d = drive_uncertainty_factors['kr']
-    sigma_alpha = drive_uncertainty_factors['kl']
+    @staticmethod
+    def calculate_positions(start_position, steps):
+        """
+        Calculate position regarding to taken steps/rotations
 
-    covariance_matrix_x = [[sigma_d,           0],
-                           [0,       sigma_alpha]]
+        :param start_position: start position (dict with 'x' & 'y')
+        :param steps: movement, a dict with
+                        ('move' ... movement in x direction of moving robot
+                         'rotation_degrees' ... rotation in degrees)
+        :return: a list of positions: position is dict with 'x', 'y', 'movement' and 'alpha'
+        """
+        theta_rads = 0
+        previous_x = start_position[POS_X]
+        previous_y = start_position[POS_Y]
+        positions = []
 
-    g_x = [[math.cos(alpha), -wheel_distance * math.sin(alpha)],
-           [math.sin(alpha), wheel_distance * math.cos(alpha)]]
+        for step in steps:
+            theta_rads = theta_rads + math.radians(step[STEP_ROT_DEGREES])
+            position = dict()
+            position[POS_X] = previous_x + step[STEP_MOVE] * math.cos(theta_rads)
+            position[POS_Y] = previous_y + step[STEP_MOVE] * math.sin(theta_rads)
+            position[POS_ALPHA] = theta_rads
+            position[POS_MOVED] = step[STEP_MOVE]
+            positions.append(position)
+            previous_x = position[POS_X]
+            previous_y = position[POS_Y]
 
-    covariance_matrix = np.matmul(np.matmul(g_x, covariance_matrix_x), np.transpose(g_x))
-    result = dict()
+        return positions
 
-    result['width'] = 10
-    result['height'] = 10
-    result['angle'] = 0
+    @staticmethod
+    def calculate_error_ellipse(drive_uncertainty_factors, wheel_distance, movement, alpha):
+        """
+        Calculate error ellipse, and accumulate covariance in sigma_p_n
 
-    last_covariance_matrix = covariance_matrix
-    return result
+        :param drive_uncertainty_factors: the factors
+        :param wheel_distance: wheel distance
+        :param movement: desired movement
+        :param alpha: rotation
+        :return: a dict containing 'width', 'height' and 'angle'
+        """
+        Movement.sigma_p_n = calculate_sigma_p_n_1(Movement.sigma_p_n, drive_uncertainty_factors, wheel_distance, movement, alpha)
+        return extract_ellipse(Movement.sigma_p_n)
